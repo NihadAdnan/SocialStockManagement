356904c0416fb31f9fa9755f08c9cc499a422ba7
Nihad Adnan <nihad08adnan@gmail.com>
2026-03-24 12:16:18 +0600

    Stock post api implemented

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
+using api.DTOs.Stock;
+using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
@@ -20,7 +22,7 @@ namespace api.Controllers
         [HttpGet]
         public IActionResult GetAll()
         {
-            var stocks = _context.Stocks.ToList();
+            var stocks = _context.Stocks.ToList().Select(s => s.ToStockDTO());
             return Ok(stocks);
         }
 
@@ -34,7 +36,16 @@ namespace api.Controllers
                 return NotFound();
             }
 
-            return Ok(stock);
+            return Ok(stock.ToStockDTO());
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] CreateStockRequestDTO createStockDTO)
+        {
+            var stockDTO = createStockDTO.ToStockFromCreateStockDTO();
+            _context.Stocks.Add(stockDTO);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetById), new { id = stockDTO.Id }, stockDTO.ToStockDTO());
         }
     }
 }
