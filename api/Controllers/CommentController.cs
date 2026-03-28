2a1b40d6aa3a59b2454f00477ddfdaf3344bd38a
Nihad Adnan <nihad08adnan@gmail.com>
2026-03-28 17:20:10 +0600

    getcomments implemented and bind with stock done

---
+++ api/Controllers/CommentController.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.DTOs.Comment;
+using api.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using api.Mappers;
+
+namespace api.Controllers
+{
+    [Route("api/comment")]
+    [ApiController]
+    public class CommentController : ControllerBase
+    {
+        private readonly ICommentRepository _commentRepository;
+        public CommentController(ICommentRepository commentRepository)
+        {
+            _commentRepository = commentRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var comments = await _commentRepository.GetAllAsync();
+            var commentDTO = comments.Select(s=> s.ToCommentDTO());
+            return Ok(commentDTO);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById([FromRoute] int id)
+        {
+            var comment = await _commentRepository.GetByIdAsync(id);
+
+            if(comment == null)
+            {
+              return NotFound();   
+            }
+
+            return Ok(comment.ToCommentDTO());
+        }
+    }
+}
